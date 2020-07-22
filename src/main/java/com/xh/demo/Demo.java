c62d77e68a6ad0a0259be9fb2c371badcbc47674
18858298383@163.com <wwJ870128>
2020-07-22 10:42:24 +0800

    first commit

---
+++ src/main/java/com/xh/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.xh.demo;
+
+/**
+ * @ClassName Demo
+ * @Description: TODO
+ * @Author Sniper
+ * @Date 2020/7/22 10:39
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
